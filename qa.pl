ee106842bb9bc5f28db54c0359b2851290d9924a
Kevin Scannell <kscanne@gmail.com>
2018-03-16 15:48:11 -0500

    QA script

---
+++ qa.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use utf8;
+
+binmode STDIN, ":utf8";
+binmode STDOUT, ":utf8";
+binmode STDERR, ":utf8";
+
+my %poshash = (
+	'nm' => 'fir',
+	'nf' => 'bain',
+	'a' => 'aid',
+	'v' => 'br',
+	's' => 'af',
+	'adv' => 'db',
+	'prep' => 'rf',
+	'pref' => 'rm',
+	'pron' => 'fa',
+	'conj' => 'cón',
+	'npl' => 'iol',
+	'u' => '',
+	'interr' => 'ceist',
+	'excl' => 'int',
+	'poss' => 'aid',
+	'ord' => 'ord',
+	'pronm' => '',
+	'card' => 'uimh',
+	'art' => 'alt',
+	'NONE' => '',
+);
+
+open(DROICHEAD, "<:utf8", "droichead.tsv") or die "Could not open droichead.tsv: $!";
+while (<DROICHEAD>) {
+	chomp;
+	my @pieces = split(/\t/);
+	print "wrong number of fields on line $.\n" unless (scalar(@pieces)==5);
+	(my $ig, my $fgb, my $pos, my $num, my $edil) = split(/\t/);
+	my $igw = $ig;
+	my $igpos = 'NONE';
+	if ($igw =~ m/_(.*)$/) {
+		$igpos = $1;
+		$igw =~ s/_.*//;
+	}
+	print "bad eDIL ref on line $.: $num\n" unless ($num =~ m/^[1-9][0-9]*$/ and $num < 44000);
+	print "weird eDIL headword on line $.: $edil\n" unless (length($edil)>0);
+	print "unknown POS on line $.: $igpos\n" unless (exists($poshash{$igpos}));
+	print "POS strings don't match on line $.: $igpos vs $pos\n" unless (exists($poshash{$igpos}) and $poshash{$igpos} eq $pos);
+	$fgb =~ s/[.^].*//;
+	$igw =~ s/[0-9]$//;
+	print "IG/FGB words don't match on line $.: $igw vs $fgb\n" unless ($fgb eq $igw or $fgb eq 'Luan');
+}
+close DROICHEAD;
+
+exit 0;
